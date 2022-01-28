941590a66e9e646a1ade3f7b36ebb4c7e2f0b9d6
Jérémy Woirhaye <jerem.woirhaye@gmail.com>
2022-01-28 18:31:38 +0100

    <add> Commencement de l'algo recursive backtracker

--- src/mazegame/generation/RecursiveBacktracker.java
+++ src/mazegame/generation/RecursiveBacktracker.java
@@ -1,11 +1,38 @@
 package mazegame.generation;
 
+import java.util.List;
+import java.util.Stack;
+
+import mazegame.Cell;
 import mazegame.Maze;
 
 public class RecursiveBacktracker implements GenerationAlgorithm {
-
+	
+	private final int seedX = 0;
+	private final int seedY = 0;
+	private Stack<Cell> stack = new Stack<Cell>();
+	
 	public void generation(Maze maze) {
-
+		
+		Cell startCell = maze.getCell(this.seedX, this.seedY);
+		startCell.setVisited();
+		this.stack.push(startCell);
+		
+		while (this.stack.empty()) {
+			Cell currentCell = this.stack.pop();
+			List<Cell> unvisitedNeightboursCells = maze.getUnvisitedNeighborsCells(currentCell);
+			
+			/**
+			 * Si il y a une voisine non visité:
+			 * 	on choisi une cellule au hasard (Collection.shuffle ?)
+			 * 	on detruit le mur entre les 2
+			 * 	on ajoute à la pile
+			 * 
+			 * sinon:
+			 * 	on retire la premiere cell de la pile
+			 */
+		}
+	
 	}
 
 }
