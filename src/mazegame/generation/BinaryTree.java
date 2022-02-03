4a3de3ceb9d1133e62bf9a0e57c59bc5e8b531a1
Jérémy Woirhaye <jerem.woirhaye@gmail.com>
2022-02-03 23:20:43 +0100

    <debug> Ajout de modification pour passe le test de labyrinthe parfait
    Ajout de modification pour que le test de labyrinthe parfait fonctionne
    sur cette algorithme de génération il fallait juste bien detruire le mur
    de jonction des cellules pour les deux cellules

--- src/mazegame/generation/BinaryTree.java
+++ src/mazegame/generation/BinaryTree.java
@@ -7,6 +7,8 @@ import mazegame.*;
  */
 public class BinaryTree implements GenerationAlgorithm {
 
+	private Maze maze;
+	
 	/**
 	 * Algorithme de Génération : Arbre Binaire
 	 * 
@@ -14,6 +16,7 @@ public class BinaryTree implements GenerationAlgorithm {
 	 */
 	public void generation(Maze maze) {
 
+		this.maze = maze;
 		int mazeHeight = maze.getHeight();
 		int mazeWidth = maze.getWidth();
 
@@ -26,11 +29,46 @@ public class BinaryTree implements GenerationAlgorithm {
 
 				// Condition de suppresion des walls
 				if ((random_int == 1 || y == mazeHeight - 1) && x < mazeWidth - 1) {
-					currentCell.eraseWall(Direction.E);
+					this.carvePath(currentCell, this.getCellWithDirection(currentCell, Direction.E));
 				} else if (y != mazeHeight - 1) {
-					currentCell.eraseWall(Direction.S);
+					this.carvePath(currentCell, this.getCellWithDirection(currentCell, Direction.S));
 				}
 			}
 		}
+	}
+	
+
+	/**
+	 * Détruit les murs entre deux cellule.
+	 * @param currentCell La cellule actuelle.
+	 * @param nextCell La cellule suivante.
+	 */
+	public void carvePath(Cell currentCell, Cell nextCell) {
+		Direction directionNextCell = Direction.directionOf(currentCell, nextCell);
+		currentCell.eraseWall(directionNextCell);
+		Direction directionCurrentCell = Direction.directionOf(nextCell, currentCell);
+		nextCell.eraseWall(directionCurrentCell);
+
+		// uncomment for step by step
+		// System.out.println(directionNextCell);
+
+	}
+	
+	public Cell getCellWithDirection(Cell cell, Direction direction) {
+
+		int x = cell.getX();
+		int y = cell.getY();
+
+		if (direction == Direction.N)
+			return this.maze.getCell(x, y - 1);
+
+		if (direction == Direction.S)
+			return this.maze.getCell(x, y + 1);
+
+		if (direction == Direction.O)
+			return this.maze.getCell(x - 1, y);
+
+		return this.maze.getCell(x + 1, y);
+
 	}
 }
