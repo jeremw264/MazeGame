11b87ef323f79847859046320c09b701539359d3
Jérémy Woirhaye <jeremy.woirhaye.etu@univ-lille.fr>
2022-02-04 18:23:20 +0100

    <bug> Si le labyrinthe n'est pas parfait il ne parcours pas toute les case accessible

--- test/mazegame/generation/GenerationAlgorirthmTest.java
+++ test/mazegame/generation/GenerationAlgorirthmTest.java
@@ -17,10 +17,16 @@ public class GenerationAlgorirthmTest {
 	protected int mazeWidth, mazeHeigth;
 	protected GenerationAlgorithm algorithm;
 
-	public GenerationAlgorirthmTest(GenerationAlgorithm algorithm) {		
+	public GenerationAlgorirthmTest(GenerationAlgorithm algorithm) {
 		this.algorithm = algorithm;
 	}
 
+	public GenerationAlgorirthmTest() {
+		this.algorithm = new BinaryTree();
+	}
+
+	// See for before class or search initializationError
+	
 	@Before
 	public void setUp() throws Exception {
 		this.mazeWidth = 5;
@@ -32,20 +38,18 @@ public class GenerationAlgorirthmTest {
 	public void borderIsNotOpen() {
 		for (int x = 0; x < this.mazeWidth; x++) {
 			assertTrue(this.maze.getCell(x, 0).wallExist(Direction.N));
-			assertTrue(this.maze.getCell(x, this.mazeHeigth-1).wallExist(Direction.S));
+			assertTrue(this.maze.getCell(x, this.mazeHeigth - 1).wallExist(Direction.S));
 		}
 		for (int y = 0; y < this.mazeHeigth; y++) {
-			assertTrue(this.maze.getCell(0,y).wallExist(Direction.O));
-			assertTrue(this.maze.getCell(this.mazeWidth -1 ,y).wallExist(Direction.E));
+			assertTrue(this.maze.getCell(0, y).wallExist(Direction.O));
+			assertTrue(this.maze.getCell(this.mazeWidth - 1, y).wallExist(Direction.E));
 		}
 	}
-	
+
 	@Test
 	public void verifyPerfectMaze() {
 		Stack<Cell> stack = new Stack<>();
 		assertTrue(false);
 	}
-	
-	
 
 }
