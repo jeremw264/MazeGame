268030f2835ad2c55852c57abe8e753d1a319606
Jérémy Woirhaye <jeremy.woirhaye.etu@univ-lille.fr>
2022-02-09 14:30:13 +0100

    <maj> Ajout des modifications pour implémentation avec Grid

--- test/mazegame/generation/GenerationAlgorirthmTest.java
+++ test/mazegame/generation/GenerationAlgorirthmTest.java
@@ -6,11 +6,12 @@ import org.junit.Before;
 import org.junit.Test;
 
 import mazegame.Direction;
+import mazegame.Grid;
 import mazegame.Maze;
 
 public class GenerationAlgorirthmTest {
 
-	protected Maze maze;
+	protected Grid grid;
 	protected int mazeWidth, mazeHeigth;
 	protected GenerationAlgorithm algorithm;
 
@@ -28,18 +29,18 @@ public class GenerationAlgorirthmTest {
 	public void setUp() throws Exception {
 		this.mazeWidth = 5;
 		this.mazeHeigth = 5;
-		this.maze = new Maze(this.mazeWidth, this.mazeHeigth, this.algorithm);
+		this.grid = new Maze(this.mazeWidth, this.mazeHeigth, this.algorithm).getGrid();
 	}
 
 	@Test
 	public void borderIsNotOpen() {
 		for (int x = 0; x < this.mazeWidth; x++) {
-			assertTrue(this.maze.getCell(x, 0).wallExist(Direction.N));
-			assertTrue(this.maze.getCell(x, this.mazeHeigth - 1).wallExist(Direction.S));
+			assertTrue(this.grid.getCell(x, 0).wallExist(Direction.N));
+			assertTrue(this.grid.getCell(x, this.mazeHeigth - 1).wallExist(Direction.S));
 		}
 		for (int y = 0; y < this.mazeHeigth; y++) {
-			assertTrue(this.maze.getCell(0, y).wallExist(Direction.O));
-			assertTrue(this.maze.getCell(this.mazeWidth - 1, y).wallExist(Direction.E));
+			assertTrue(this.grid.getCell(0, y).wallExist(Direction.O));
+			assertTrue(this.grid.getCell(this.mazeWidth - 1, y).wallExist(Direction.E));
 		}
 	}
 
