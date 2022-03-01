499ff7cd97b7236880fb8337cf9a8c39bb998256
beyaert <beyaert@a16p8.fil.univ-lille1.fr>
2022-03-01 10:05:42 +0100

    # WARNING: head commit changed in the meantime
    <maj>DivisionRecursive

---
+++ src/mazegame/generation/RecursiveDivision.java
@@ -0,0 +1,39 @@
+package mazegame.generation;
+
+import java.util.Random;
+
+import mazegame.Grid;
+
+public class RecursiveDivision extends GenerationAlgorithm
+{
+	
+	@Override
+	public Grid generation(int width, int height) 
+	{
+		this.grid = new Grid(width, height, false);	
+		return this.grid;
+	}
+	
+	private void divide(int x, int y, int width, int height)
+	{
+		if (width <= 1 && height <= 1)
+		{
+			return;
+		}
+		else 
+		{
+			int c = (int)Math.random();
+			if (c == 0) // mur horizontale
+			{
+				
+			}
+			else // mur verticale
+			{
+				
+			}
+		}
+	}
+		
+}
+
+
