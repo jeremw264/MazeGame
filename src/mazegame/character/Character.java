ef913262b37401356efa3f25d51f14aac78b38de
beyaert <franck.beyaert.etu@univ-lille.fr>
2022-03-26 17:12:02 +0100

    <maj> Class abstraite Character

--- src/mazegame/character/Character.java
+++ src/mazegame/character/Character.java
@@ -1,5 +1,39 @@
 package mazegame.character;
 
+import java.util.List;
+
+import mazegame.Cell;
+import mazegame.Map;
+import mazegame.item.Item;
+
 public abstract class Character {
 
+		private int x;
+		private int y;
+		private Cell currentCell;
+		private Map map;
+		
+		private List<Item> inventory;
+		
+		public Character(int x, int y) {
+			this.x = x;
+			this.y = y;
+		}
+		
+		public int getY() {
+			return y;
+		}
+		
+		public int getX() {
+			return x;
+		}
+		
+		public void setCell(Cell nextCell) {
+			
+		}
+		
+		public Cell getCell() {
+			return currentCell;
+			
+		}
 }
