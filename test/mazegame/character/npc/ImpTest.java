7ca1c0e0e353da51ce3da0a25a587ab5ad3ffbc7
Jérémy Woirhaye <jeremy.woirhaye.etu@univ-lille.fr>
2022-04-19 21:36:16 +0200

    <maj> Correction des tests pour implémentation de move

--- test/mazegame/character/npc/ImpTest.java
+++ test/mazegame/character/npc/ImpTest.java
@@ -1,7 +1,10 @@
 package mazegame.character.npc;
 
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Before;
 
+import mazegame.action.Move;
 import mazegame.character.NpcTest;
 
 public class ImpTest extends NpcTest {
@@ -9,5 +12,11 @@ public class ImpTest extends NpcTest {
 	@Before
 	public void setUp() {
 		this.npc = new Imp(this.x,this.y,this.map);
+	}
+	
+	@Override
+	public void getActionTest() {
+		assertTrue(this.npc.getAction() instanceof Move);
+
 	}
 }
