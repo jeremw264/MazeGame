f915bfd638ac9873826ee26a05637d6ee54559fb
beyaert <franck.beyaert.etu@univ-lille.fr>
2022-04-21 21:28:36 +0200

    <add> Ajout de la classe Action Talk

---
+++ src/mazegame/action/Talk.java
@@ -0,0 +1,19 @@
+package mazegame.action;
+
+import mazegame.Game;
+import mazegame.character.Character;
+import mazegame.character.Player;
+import mazegame.character.npc.*;
+import mazegame.item.*;
+
+import org.json.*;
+
+
+public class Talk extends Action {
+	@Override
+	public void run(Character character) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
