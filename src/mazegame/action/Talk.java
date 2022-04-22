7253e92142b200879f77c7ff29a3258047649fec
beyaert <franck.beyaert.etu@univ-lille.fr>
2022-04-22 09:48:34 +0200

    <add> Action Talk

--- src/mazegame/action/Talk.java
+++ src/mazegame/action/Talk.java
@@ -2,17 +2,15 @@ package mazegame.action;
 
 import mazegame.Game;
 import mazegame.character.Character;
-import mazegame.character.Player;
-import mazegame.character.npc.*;
-import mazegame.item.*;
-
-import org.json.*;
+import org.json.JSONObject;
+import org.json.JSONArray;
 
 
 public class Talk extends Action {
 	@Override
 	public void run(Character character) {
 		// TODO Auto-generated method stub
+		
 		
 	}
 
