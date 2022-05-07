c90c58cd9144b2a30f0805c3e9b9c8f703305197
Jérémy Woirhaye <jerem.woirhaye@gmail.com>
2022-05-07 02:34:23 +0200

    <test> maj pour gameBuilder

--- tests/mazegame/challenge/WaitRoundTest.java
+++ tests/mazegame/challenge/WaitRoundTest.java
@@ -12,21 +12,22 @@ import mazegame.Map;
 public class WaitRoundTest {
 
 	private Challenge challenge;
+	private Player player;
 	
 	@Before
 	public void setUp() throws Exception {
 		Map map = new Map(5, 5);
-		Player player = new Hero(0, 0, map);
-		this.challenge = new WaitRound(player, 3);
+		this.player = new Hero(0, 0, map);
+		this.challenge = new WaitRound( 3);
 	}
 
 	@Test
 	public void waitManyRounds() {
-		assertFalse(this.challenge.isCheck());
-		assertFalse(this.challenge.isCheck());
-		assertFalse(this.challenge.isCheck());
+		assertFalse(this.challenge.isCheck(this.player));
+		assertFalse(this.challenge.isCheck(this.player));
+		assertFalse(this.challenge.isCheck(this.player));
 
-		assertTrue(this.challenge.isCheck());
+		assertTrue(this.challenge.isCheck(this.player));
 	}
 
 }
