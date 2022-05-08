7cd78037f8824579bc8c166e6620ac57ae3d9f2b
Jérémy Woirhaye <jerem.woirhaye@gmail.com>
2022-05-08 17:42:38 +0200

    <maj> Affichage des items et personnage sur la case

--- src/mazegame/action/LookAround.java
+++ src/mazegame/action/LookAround.java
@@ -8,6 +8,7 @@ import mazegame.Game;
 import mazegame.State;
 import mazegame.character.Character;
 import mazegame.character.Player;
+import mazegame.item.Item;
 
 /**
  * Classe LookAround
@@ -27,23 +28,61 @@ public class LookAround extends Action {
 			return State.Exit;
 		}
 
-		Game.DISPLAYER.displayMap(character.getMap());
+		this.displayHeaderData(character);
+		this.displayDirectionData(character);
+		this.displayNpcData(character);
+		this.displayItemData(character);
+
+		return State.Ok;
+
+	}
+
+	private void displayHeaderData(Character character) {
 
 		final int currentX = character.getX();
 		final int currentY = character.getY();
 
+		Game.DISPLAYER.displayMap(character.getMap());
+		Game.DISPLAYER.displayMsg("Vous êtes sur la case (" + currentX + "," + currentY + ").");
+		Game.DISPLAYER.displayMsg("Vous avez " + character.getCoins() + " or sur vous.");
+
+	}
+
+	private void displayDirectionData(Character character) {
 		List<String> directionNameList = new LinkedList<String>();
 
 		for (Direction direction : character.getAccessibleDirections()) {
 			directionNameList.add(direction.toString());
 		}
 
-		Game.DISPLAYER.displayMsg("Vous êtes sur la case (" + currentX + "," + currentY + ")");
-		Game.DISPLAYER.displayMsg("Vous avez " + character.getCoins() + " or sur vous.");
 		Game.DISPLAYER.displayChoise("Les directions accessible sont : ", directionNameList);
 
-		return State.Ok;
+	}
 
+	private void displayNpcData(Character character) {
+		List<String> npcsNameList = new LinkedList<String>();
+
+		for (Character c : character.getCell().charactersList()) {
+			if (!(c instanceof Player)) {
+				npcsNameList.add(c.toString());
+			}
+		}
+
+		if (npcsNameList.size() > 0) {
+			Game.DISPLAYER.displayChoise("Voici les personages autour de vous :", npcsNameList);
+		}
+	}
+
+	private void displayItemData(Character character) {
+		List<String> itemsNameList = new LinkedList<String>();
+
+		for (Item item : character.getCell().getItemList()) {
+			itemsNameList.add(item.toString());
+		}
+
+		if (itemsNameList.size() > 0) {
+			Game.DISPLAYER.displayChoise("Voici les objets autour de vous :", itemsNameList);
+		}
 	}
 
 }
