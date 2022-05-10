9bb2864ea4af2d51802f08b460f6fc47e82fa332
Jérémy Woirhaye <jerem.woirhaye@gmail.com>
2022-05-10 09:34:18 +0200

    <add> UserInteraction choix avec description

--- src/mazegame/utils/UserInteration.java
+++ src/mazegame/utils/UserInteration.java
@@ -56,5 +56,55 @@ public class UserInteration {
 		return responceObjects;
 
 	}
+	
+	public static Map<String, Object> getChoise(String firstSentence, List<String> listOfChoises,List<String> descriptionList, boolean canCancel) {
+
+		Map<String, Object> responceObjects = new HashMap<>();
+		responceObjects.put("STATE", State.Ok);
+
+		List<String> choises = new LinkedList<>();
+
+		// Ajouter le choix de pouvoir revenir en arrière
+		if (canCancel) {
+			choises.add(UserInteration.RETURNWORD_STRING);
+		}
+		
+		List<String> listOfChoisesAndDescription = new LinkedList<>();
+		
+		for (int i = 0; i < listOfChoises.size(); i++) {
+			listOfChoisesAndDescription.add(listOfChoises.get(i)+" "+descriptionList.get(i));
+			
+		}
+
+		choises.addAll(listOfChoises);
+
+		String choise;
+
+		do {
+
+			Game.DISPLAYER.displayMsg("--------------------------------------------------");
+
+			Game.DISPLAYER.displayChoise(firstSentence, listOfChoisesAndDescription);
+
+			choise = Game.INPUT.getString().toLowerCase().strip();
+
+			if (choise.equals(UserInteration.EXITWORD_STRING)) {
+				responceObjects.put("STATE", State.Exit);
+				break;
+			} else if (choise.equals(UserInteration.RETURNWORD_STRING)) {
+				responceObjects.put("STATE", State.Cancel);
+			}
+
+			if (!choises.contains(choise)) {
+				Game.DISPLAYER.displayError(UserInteration.INVALIDCHOISE_STRING);
+			}
+
+		} while (!choises.contains(choise));
+
+		responceObjects.put("choice", choise);
+
+		return responceObjects;
+
+	}
 
 }
